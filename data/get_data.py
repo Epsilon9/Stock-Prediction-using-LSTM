2f1cbc37ab1cc94dbfac0f9833986e6c70906cdb
SaiSaran <saisaranb19@gmail.com>
2023-06-21 11:46:44 +0530

    Updated and Added LSTM model

--- data/get_data.py
+++ data/get_data.py
@@ -1,3 +1,12 @@
+import requests
+import pandas as pd
+
+def get_ticker(stri):
+    url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={stri}&apikey=DJ5ATAVVQLJE2IFG'
+    r = requests.get(url)
+    data = r.json()
+    return str(pd.DataFrame(data["bestMatches"]).iloc[0][0])
+
 def get_data(ticker="TIME_SERIES_INTRADAY",sym="IBM"):
     text="TIME_SERIES_INTRADAY"
     url = f"https://www.alphavantage.co/query?function={ticker}&symbol={sym}&interval=5min&apikey=DJ5ATAVVQLJE2IFG"
@@ -12,3 +21,14 @@ def clean_col_names(df):
         for i in df.columns:
             df[i]=pd.to_numeric(df[i])
     return df
+
+def get_details(data):
+    print(f'The {data["Meta Data"].get("1. Information")} with stock = {data["Meta Data"].get("2. Symbol")} was last refreshed at {data["Meta Data"].get("3. Last Refreshed")} with timezone = {data["Meta Data"].get("5. Time Zone")}')
+
+data=get_data(ticker="TIME_SERIES_DAILY_ADJUSTED",sym=get_ticker(input()))
+to_get=list(data)[1]
+
+df = pd.DataFrame(data[to_get]).T
+
+clean_col_names(df)
+get_details(data)
